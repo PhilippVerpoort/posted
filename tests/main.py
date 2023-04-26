557316b652af0fd051104e18e0817351844a9331
Philipp Verpoort <philipp.verpoort@pik-potsdam.de>
2023-04-26 13:58:28 +0200

    Some testing files.

---
+++ tests/main.py
@@ -0,0 +1,11 @@
+from src.python.read.read_config import techs
+from src.python.ted.TEDataSet import TEDataSet
+
+
+for tid in ['direct-reduction']:
+    #if techs[tid]['class'] != 'conversion': continue
+
+    ted = TEDataSet(tid).load()
+    dataset = ted.getDataset()
+    selected = ted.selectData([2020, 2030, 2040, 2050])
+    print(selected)
---
+++ tests/units.py
@@ -0,0 +1,8 @@
+from src.python.read.read_config import flowTypes
+from src.python.units.units import ureg, convUnit
+
+
+# test conversion
+#print(ureg('1 kg').to('MWh', 'flocon', energycontent=ureg('33.33 kWh/kg')))
+print(convUnit('kW', 't/a', flowTypes['h2']))
+# print(convUnit('pct', 'dimensionless'))
