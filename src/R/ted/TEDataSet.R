d371c518476bacdc7485d36219cb8bcb46da0782
Philipp Verpoort <philipp.verpoort@pik-potsdam.de>
2023-05-04 00:17:39 +0200

    Code refactoring in R.

--- src/R/ted/TEDataSet.R
+++ src/R/ted/TEDataSet.R
@@ -1,16 +1,26 @@
 source("src/R/path.R")
 source("src/R/read/read_config.R")
 
+library(dplyr)
 
-loadDataset <- function(tid, load_other = c(), load_default = TRUE) {
+
+loadDataset <- function(tid, load_other = list(), load_default = TRUE) {
+    loadPaths <- load_other
+    if (load_default) {
+        loadPaths <- append(loadPaths, pathOfTEDFile(tid))
+    }
 
     # read TED data from CSV files
     mapColnamesDtypes <- list()
     for (colType in names(dataFormats)) {
         mapColnamesDtypes[[colType]] <- dataFormats[[colType]]$dtype
     }
+    datasetList <- list()
+    for (i in seq_along(loadPaths)) {
+        p <- loadPaths[[i]]
+        datasetList[[i]] <- readTEDFile(p, mapColnamesDtypes)
+    }
+    dataset <- dplyr::bind_rows(datasetList)
 
-    p <- pathOfTEDFile(tid)
-    dataset <- readTEDFile(p, mapColnamesDtypes)
     return(dataset)
 }
